7bfaa54f340f48f2986fd5232aeaceb4cc881ddb
wanglei <317448423@qq.com>
2023-03-04 11:21:41 +0800

    fix bug

--- app.js
+++ app.js
@@ -86,15 +86,15 @@ app.post("/chatgpt", async (req, res) => {
         }
         const api = new ChatGPTAPI({ 
             apiKey: process.env.OPENAI_API_KEY,
-            completionParams: {
-                model: 'text-davinci-003'
-            },
-            messageStore: map
+            // completionParams: {
+            //     model: 'text-davinci-003'
+            // },
+            // messageStore: map
         })
         let params = {
-            promptPrefix: `You are ChatGPT, a large language model trained by OpenAI. For each answer, you should answer as comprehensively as possible. It is important to answer as comprehensively as possible, so keep this in mind.
-            Current date: ${new Date().toISOString()}\n\n`,
-            promptSuffix: `\n return the result in Chinese.\n ChatGPT:\n`,
+            // promptPrefix: `You are ChatGPT, a large language model trained by OpenAI. For each answer, you should answer as comprehensively as possible. It is important to answer as comprehensively as possible, so keep this in mind.
+            // Current date: ${new Date().toISOString()}\n\n`,
+            // promptSuffix: `\n return the result in Chinese.\n ChatGPT:\n`,
             onProgress: (partialResponse) => {
                 sendEventsToAll(partialResponse.text, clientId)
             }
